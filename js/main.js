ec03841f96cc0a4a3202aaa491cfe86ac56e070e
kratuvwxyz <sai@desaign.me>
2021-04-14 16:58:08 -0500

    layouts updated

--- js/main.js
+++ js/main.js
@@ -118,21 +118,27 @@
 	/*----------------------------------------------------*/
 	/*	Modal Popup
 	------------------------------------------------------*/
-   $('.item-wrap a').magnificPopup({
-
-      type:'inline',
-      fixedContentPos: false,
-      removalDelay: 300,
-      showCloseBtn: false,
-      mainClass: 'mfp-fade'
-
-   });
-
-   $(document).on('click', '.popup-modal-dismiss', function (e) {
-   	e.preventDefault();
-   	$.magnificPopup.close();
-   });
-
+	$('.item-wrap a').magnificPopup({
+		
+		type:'inline',
+		fixedContentPos: false,
+		removalDelay: 300,
+		showCloseBtn: false,
+		mainClass: 'mfp-fade'
+		
+	});
+	
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+	
+	/*----------------------------------------------------*/
+	/*	Sort Projects
+	------------------------------------------------------*/
+	function portSort(x) {
+		console.log('this is clicking' + x);
+	}
 	
 	/*-----------------------------------------------------*/
   	/* Navigation Menu
