3eba11866ecbd2aec7387765e7ea992b2a8fc29e
kratuvwxyz <sai@desaign.me>
2025-01-23 23:41:09 -0600

    navigation fragment moved

--- js/fragments.js
+++ js/fragments.js
@@ -140,6 +140,79 @@ Denim Summit
 // Wait for all fragments to load before running additional code
 Promise.all(fragmentPromises).then(() => {
   console.log("All fragments loaded.");
+
+
+  /*-----------------------------------------------------*/
+  /* Navigation Menu
+   ------------------------------------------------------ */
+  var toggleButton = $(".menu-toggle"),
+    nav = $(".main-navigation");
+
+  // toggle button
+  toggleButton.on("click", function (e) {
+    e.preventDefault();
+    toggleButton.toggleClass("is-clicked");
+    nav.slideToggle();
+  });
+
+  // nav items
+  nav.find("li a").on("click", function () {
+    // update the toggle button
+    toggleButton.toggleClass("is-clicked");
+    // fadeout the navigation panel
+    nav.fadeOut();
+  });
+
+  /*---------------------------------------------------- */
+  /* Highlight the current section in the navigation bar
+    ------------------------------------------------------ */
+  var sections = $("section"),
+    navigation_links = $("#main-nav-wrap li a");
+
+  sections.waypoint({
+    handler: function (direction) {
+      var active_section;
+
+      active_section = $("section#" + this.element.id);
+
+      if (direction === "up") active_section = active_section.prev();
+
+      var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');
+
+      navigation_links.parent().removeClass("current");
+      active_link.parent().addClass("current");
+    },
+
+    offset: "25%",
+  });
+
+  /*---------------------------------------------------- */
+  /* Smooth Scrolling
+    ------------------------------------------------------ */
+  $(".smoothscroll").on("click", function (e) {
+    e.preventDefault();
+
+    var target = this.hash,
+      $target = $(target);
+
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $target.offset().top,
+        },
+        2500,
+        "swing",
+        function () {
+          window.location.hash = target;
+        }
+      );
+  });
+
+  /*---------------------------------------------------- */
+  /* Portfolio
+    ------------------------------------------------------ */
+
   // Fashion Floors
   createPortfolioItem({
     id: "Fashion-Floors",
